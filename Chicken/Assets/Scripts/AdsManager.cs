704b57696add28a5321b5613fdfde22f5472366c
Wei <awei1127@hotmail.com>
2020-08-28 16:36:13 +0800

    正式發布版本0.3
    廣告還不確定能否正常顯示

---
+++ Chicken/Assets/Scripts/AdsManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using EasyMobile;
+
+public class AdsManager : MonoBehaviour
+{
+    
+    void Awake()
+    {
+        if (!RuntimeManager.IsInitialized())
+            RuntimeManager.Init();
+    }
+
+    public void ShowBannerAds()
+    {
+        Advertising.ShowBannerAd(BannerAdPosition.Bottom);
+    }
+
+    public void ShowInterstitialAds()
+    {
+        if (Advertising.IsInterstitialAdReady())
+            Advertising.ShowInterstitialAd();
+    }
+
+    public void ShowRewardedAds()
+    {
+        if (Advertising.IsRewardedAdReady())
+            Advertising.ShowRewardedAd();
+    }
+}
