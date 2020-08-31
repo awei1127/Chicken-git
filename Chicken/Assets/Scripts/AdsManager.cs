fe54a3bbf9894228ca9e5f06ef14201c0035c7b1
Wei <awei1127@hotmail.com>
2020-08-31 22:50:41 +0800

    刪除easy mobile直接用admob SDK成功導入廣告！爽！

--- Chicken/Assets/Scripts/AdsManager.cs
+++ Chicken/Assets/Scripts/AdsManager.cs
@@ -2,31 +2,123 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using EasyMobile;
+using GoogleMobileAds.Api;
 
 public class AdsManager : MonoBehaviour
 {
-    
-    void Awake()
+    private BannerView bannerView;
+    private InterstitialAd interstitial;
+    private RewardedAd rewarded;
+    public Text interText;
+    public Text rewardText;
+    // Start is called before the first frame update
+    void Start()
     {
-        if (!RuntimeManager.IsInitialized())
-            RuntimeManager.Init();
+        // Initialize the Google Mobile Ads SDK.
+        MobileAds.Initialize(initStatus => { });
+        RequestInterstitial();
+        RequestRewarded();
     }
 
-    public void ShowBannerAds()
+    private void Update()
     {
-        Advertising.ShowBannerAd(BannerAdPosition.Bottom);
+        if (interstitial.IsLoaded())
+        {
+            interText.text = "interAd is READY!!";
+            interText.color = Color.green;
+        }
+        else
+        {
+            interText.text = "interAd is NOT ready!!";
+            interText.color = Color.red;
+        }
+
+        if (rewarded.IsLoaded())
+        {
+            rewardText.text = "rewardedAd is READY!!";
+            rewardText.color = Color.green;
+        }
+        else
+        {
+            rewardText.text = "rewardedAd is NOT ready!!";
+            rewardText.color = Color.red;
+        }
     }
 
-    public void ShowInterstitialAds()
+    public void RequestBanner()
     {
-        if (Advertising.IsInterstitialAdReady())
-            Advertising.ShowInterstitialAd();
+        #if UNITY_ANDROID
+            string adUnitId = "ca-app-pub-3940256099942544/6300978111";
+
+        #elif UNITY_IPHONE
+            string adUnitId = "ca-app-pub-3940256099942544/2934735716";
+        #else
+            string adUnitId = "unexpected_platform";
+        #endif
+
+        // Create a 320x50 banner at the top of the screen.
+        bannerView = new BannerView(adUnitId, AdSize.Banner, AdPosition.Bottom);
+        // Create an empty ad request.
+        AdRequest request = new AdRequest.Builder().Build();
+        // Load the banner with the request.
+        bannerView.LoadAd(request);
     }
 
-    public void ShowRewardedAds()
+    public void RequestInterstitial()
     {
-        if (Advertising.IsRewardedAdReady())
-            Advertising.ShowRewardedAd();
+        #if UNITY_ANDROID
+            string adUnitId = "ca-app-pub-3940256099942544/1033173712";
+        #elif UNITY_IPHONE
+            string adUnitId = "ca-app-pub-3940256099942544/4411468910";
+        #else
+            string adUnitId = "unexpected_platform";
+        #endif
+
+        // Initialize an InterstitialAd.
+        interstitial = new InterstitialAd(adUnitId);
+        // Create an empty ad request.
+        AdRequest request = new AdRequest.Builder().Build();
+        // Load the interstitial with the request.
+        interstitial.LoadAd(request);
     }
+
+    public void RequestRewarded()
+    {
+        #if UNITY_ANDROID
+            string adUnitId = "ca-app-pub-3940256099942544/5224354917";
+        #elif UNITY_IPHONE
+            string adUnitId = "ca-app-pub-3940256099942544/1712485313";
+        #else
+            string adUnitId = "unexpected_platform";
+        #endif
+
+        // Initialize an RewardedAd.
+        rewarded = new RewardedAd(adUnitId);
+        // Create an empty ad request.
+        AdRequest request = new AdRequest.Builder().Build();
+        // Load the rewarded ad with the request.
+        rewarded.LoadAd(request);
+    }
+
+
+
+    public void ShowBannerAd()
+    {
+        RequestBanner();
+    }
+
+    public void ShowInterstitialAd()
+    {
+        if (interstitial.IsLoaded())
+            interstitial.Show();
+    }
+
+    public void ShowRewardedAd()
+    {
+        if (rewarded.IsLoaded())
+            rewarded.Show();
+    }
+
+
+
 }
